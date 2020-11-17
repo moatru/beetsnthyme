bd13369306c29230dd60bd356a601e1e6611bbef
moatru <moasteve@gmail.com>
2020-11-17 15:26:17 -0500

    fix'n it up

--- assets/main.js
+++ assets/main.js
@@ -13,59 +13,59 @@ $( document ).ready(function() {
 	// end hamburger menu
 
 
-// IMAGE SLIDSHOW HOMEPAGE
-var slideIndex = 0;
-carousel();
+// // IMAGE SLIDSHOW HOMEPAGE
+// var slideIndex = 0;
+// carousel();
+//
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 10000); // Change image every 2 seconds
+// }
+// // END SLIDESHOW
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 10000); // Change image every 2 seconds
-}
-// END SLIDESHOW
 
 
+  This is the pop-up gallery
+  	$('.popup-gallery').magnificPopup({
+  		delegate: 'a',
+  		type: 'image',
+  		tLoading: 'Loading image #%curr%...',
+  		mainClass: 'mfp-img-mobile',
+  		gallery: {
+  			enabled: true,
+  			navigateByImgClick: true,
+  			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+  		},
+  		image: {
+  			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+  			titleSrc: function(item) {
+  				return item.el.attr('title');
+  			}
+  		}
+  	});
 
-  // This is the pop-up gallery
-//   	$('.popup-gallery').magnificPopup({
-//   		delegate: 'a',
-//   		type: 'image',
-//   		tLoading: 'Loading image #%curr%...',
-//   		mainClass: 'mfp-img-mobile',
-//   		gallery: {
-//   			enabled: true,
-//   			navigateByImgClick: true,
-//   			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-//   		},
-//   		image: {
-//   			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-//   			titleSrc: function(item) {
-//   				return item.el.attr('title');
-//   			}
-//   		}
-//   	});
-//
-// 	var allPanels = $('.accordion > dd').hide();
-//
-// 		$('.accordion > dt > a').click(function() {
-//
-// 			allPanels.slideUp();
-//
-// 	    $(this).filter('.open').removeClass('open').addClass('closing')
-// 	    $('.accordion > dt > a').removeClass('open');
-// 	  $(this).not(".closing").addClass('open').parent().next().slideDown();
-// 	    $('.accordion > dt > a').removeClass('closing');
-//
-// 	    return false;
-// 		});
-//
-//
-// });
+	var allPanels = $('.accordion > dd').hide();
 
-// end accordian
+		$('.accordion > dt > a').click(function() {
+
+			allPanels.slideUp();
+
+	    $(this).filter('.open').removeClass('open').addClass('closing')
+	    $('.accordion > dt > a').removeClass('open');
+	  $(this).not(".closing").addClass('open').parent().next().slideDown();
+	    $('.accordion > dt > a').removeClass('closing');
+
+	    return false;
+		});
+
+
+});
+
+end accordian
